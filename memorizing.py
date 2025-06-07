a1b9cba17329064ee40dfaa9991425b66afd839c
Sue Raisianzadeh <raisianz@gmail.com>
2025-06-07 20:31:23 +1200

    memorizing

--- memorizing.py
+++ memorizing.py
@@ -10,19 +10,38 @@
 # It is not allowed to use Python built-in functions like reverse(), reversed(), or similar in this task.
 
 
-def solution(s):
-    result = []  # cleaned characters
+# def solution(s):
+#     result = []  # cleaned list
 
+#     for char in s:
+#         code = ord(char)
+#         if 65 <= code <= 90:  # A-Z
+#             result.append(chr(code + 32))  # convert to lowercase
+#         elif 97 <= code <= 122:  # a-z
+#             result.append(char)  # already lowercase
+#         # else: ignore non-letters
+
+#     # Check if reversed matches
+#     n = len(result)
+#     for i in range(n // 2):
+#         if result[i] != result[n - 1 - i]:
+#             return False
+#     return True
+
+
+
+def solution (s):
+    result=[]
     for char in s:
-        code = ord(char)
-        if 65 <= code <= 90:  # uppercase
-            result.append(chr(code + 32))
-        elif 97 <= code <= 122:  # lowercase
+        if 65<=ord(char)<=90:
+            result.append(chr(ord(char)+ 32))
+        elif 97<=ord(char)<=122:
             result.append(char)
-
-    # Check palindrome
-    n = len(result)
-    for i in range(n // 2):
-        if result[i] != result[n - 1 - i]:
-            return False
+    for i in range (len(result)//2):
+         if result[i]!= result[len(result)-1-i]:
+              return False
     return True
+        
+# Example usage:
+print(solution("Racecar"))  # True
+print(solution("Hello"))    # False
